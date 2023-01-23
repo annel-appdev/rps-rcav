61698baf9998fc8cd827d4930f98bbec32fbc31e
annenyc <annelau.nyc@gmail.com>
2023-01-23 00:30:10 +0000

    anne-rps-first commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,17 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+
+  def play_rock
+    #write a ton of code
+
+    #redirect_to("https://www.wikipedia.")
+    
+    #render({ :plain => rand(200) })
+
+    #render({ :html => "<h1>Howdy, world!!</h1>".html_safe })
+
+    render({ :template => "game_templates/user_rock.html.erb" })
+
+  end
 end
